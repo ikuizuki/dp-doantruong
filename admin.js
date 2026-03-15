6dbe1bdd8b514f70ef9fe5f7199acfca192ba71c
ikuizuki <ikuizukidong@gmail.com>
2026-03-16 05:07:46 +0700

    update

--- admin.js
+++ admin.js
@@ -135,18 +135,24 @@ function editActivity(id) {
 }
 
 /* DELETE */
-
 async function deleteActivity(id) {
   if (!confirm("Bạn chắc chắn muốn xoá hoạt động này?")) return;
 
-  await fetch("/api/deleteActivityapi", {
-    method: "POST",
+  const card = document.getElementById(`activity-${id}`);
 
-    headers: { "Content-Type": "application/json" },
+  card.classList.add("fade-out");
 
-    body: JSON.stringify({ id }),
-  });
+  setTimeout(async () => {
+    await fetch("/api/deleteActivity", {
+      method: "POST",
 
+      headers: { "Content-Type": "application/json" },
+
+      body: JSON.stringify({ id }),
+    });
+
+    card.remove();
+  }, 300);
   loadAdminActivities();
 }
 
