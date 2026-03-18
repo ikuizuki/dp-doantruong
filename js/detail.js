f2325522f300e1da1198147f91aa71b5d571404f
ikuizuki <ikuizukidong@gmail.com>
2026-03-18 22:07:00 +0700

    update

--- js/detail.js
+++ js/detail.js
@@ -3,42 +3,74 @@ const username = localStorage.getItem("username");
 if (!username) {
   window.location.href = "login.html";
 }
-const menu = document.getElementById("menu");
-// 👉 nếu là admin
+
+document.getElementById("hello").innerText = "Xin chào, " + username;
+
 if (username === "admin") {
-  menu.innerHTML += `
-    <a href="../html/admin.html" style="color:red; font-weight:bold;">
+  document.getElementById("menu").innerHTML += `
+    <a href="../html/admin.html" style="color:red;font-weight:bold">
       QUẢN LÝ
     </a>
   `;
 }
+
 function logout() {
   localStorage.removeItem("username");
   window.location.href = "../html/login.html";
 }
-// 👉 hiện xin chào
-const hello = document.getElementById("hello");
-if (hello) {
-  hello.innerText = "Xin chào, " + username;
-}
 
 async function loadDetail() {
   const params = new URLSearchParams(window.location.search);
-  const id = params.get("id");
+  const id = parseInt(params.get("id"));
 
   const res = await fetch("/api/getActivities");
   const data = await res.json();
 
   const a = data[id];
 
-  const div = document.getElementById("detail");
+  const detailDiv = document.getElementById("detail");
+  const relatedDiv = document.getElementById("related");
 
-  div.innerHTML = `
-    <h1>${a.title}</h1>
-    <p>📅 ${a.date} | 📍 ${a.location}</p>
-    <img src="${a.image}" style="width:400px">
-    <p>${a.description}</p>
+  if (!a) {
+    detailDiv.innerHTML = "<p>Không tìm thấy bài viết</p>";
+    return;
+  }
+
+  // DETAIL
+  detailDiv.innerHTML = `
+    <a href="thongbao.html" class="back-btn">← Quay lại</a>
+
+    <h1 class="detail-title">${a.title}</h1>
+
+    <div class="detail-meta">
+      📅 ${a.date || ""} | 📍 ${a.location || ""}
+    </div>
+
+    <img src="${a.image}" class="detail-img"
+         onerror="this.src='../pic/default.jpg'">
+
+    <p class="detail-desc">
+      ${a.description.replace(/\n/g, "<br>")}
+    </p>
   `;
+
+  // RELATED (lấy random 3 bài khác)
+  let relatedHTML = "";
+
+  data.forEach((item, index) => {
+    if (index !== id && relatedHTML.split("related-item").length <= 3) {
+      relatedHTML += `
+        <div class="related-item"
+             onclick="location.href='detail.html?id=${index}'">
+          <img src="${item.image}" 
+               onerror="this.src='../pic/default.jpg'">
+          <h4>${item.title}</h4>
+        </div>
+      `;
+    }
+  });
+
+  relatedDiv.innerHTML = relatedHTML;
 }
 
 loadDetail();
