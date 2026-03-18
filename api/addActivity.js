6a6ddf6a356b24d994bd5e4e9b801a056fe32a29
ikuizuki <ikuizukidong@gmail.com>
2026-03-18 22:41:15 +0700

    update

--- api/addActivity.js
+++ api/addActivity.js
@@ -16,6 +16,7 @@ export default async function handler(req, res) {
     description,
     location,
     image,
+    createdAt: new Date(), // 👈 BẮT BUỘC PHẢI CÓ
   });
 
   res.json({ success: true });
--- api/getActivities.js
+++ api/getActivities.js
@@ -4,7 +4,11 @@ export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db("chidoan");
 
-  const data = await db.collection("activities").find({}).toArray();
+  const data = await db
+    .collection("activities")
+    .find({})
+    .sort({ createdAt: -1 }) // 👈 thêm dòng này
+    .toArray();
 
   res.json(data);
 }
